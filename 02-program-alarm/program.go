80e8e8bf741ec24db232f4bcf07a4d2aeccfa1d8
Igor Prusov <prusovigor@gmail.com>
2020-03-27 00:51:53 +0300

    Add solution of Day 2 part 2

--- 02-program-alarm/program.go
+++ 02-program-alarm/program.go
@@ -22,11 +22,11 @@ func parseInput(input string) []int {
 	return program
 }
 
-func programRun(program []int) {
+func programRun(program []int) int {
 	for i := 0; ; i += 4 {
 		switch opcode := program[i]; opcode {
 		case 99:
-			return
+			return 0
 		case 1:
 			operand1 := program[program[i+1]]
 			operand2 := program[program[i+2]]
@@ -39,15 +39,15 @@ func programRun(program []int) {
 			program[result_index] = operand1 * operand2
 		default:
 			fmt.Fprintln(os.Stderr, "Failed to parse opcode: ", opcode)
-			os.Exit(1)
+			return -1
 
 		}
 	}
 }
 
-func restoreProgram(program []int) {
-	program[1] = 12
-	program[2] = 2
+func restoreProgram(program []int, noun int, verb int) {
+	program[1] = noun
+	program[2] = verb
 }
 
 func main() {
@@ -58,10 +58,29 @@ func main() {
 	}
 
 	inpunt_string := strings.Trim(string(input), "\n")
-	program := parseInput(inpunt_string)
-	restoreProgram(program)
+	original_program := parseInput(inpunt_string)
 
+	program := make([]int, len(original_program))
+	copy(program, original_program)
+
+	restoreProgram(program, 12, 2)
 	programRun(program)
 
 	fmt.Println("Result =", program[0])
+
+	const desired_output = 19690720
+
+	for noun := 0; noun < 100; noun++ {
+		for verb := 0; verb < 100; verb++ {
+			copy(program, original_program)
+
+			restoreProgram(program, noun, verb)
+			res := programRun(program)
+			if res == 0 && program[0] == desired_output {
+				fmt.Println("Result =", 100*noun+verb)
+				return
+			}
+		}
+	}
+	os.Exit(1)
 }
--- 02-program-alarm/program_test.go
+++ 02-program-alarm/program_test.go
@@ -7,6 +7,7 @@ import (
 type testcase struct {
 	program   []int
 	exppected []int
+	retcode   int
 }
 
 func TestProgramRun(t *testing.T) {
@@ -20,27 +21,35 @@ func TestProgramRun(t *testing.T) {
 				2, 3, 11, 0,
 				99,
 				30, 40, 50},
+			0,
 		},
 		testcase{
 			[]int{1, 0, 0, 0, 99},
 			[]int{2, 0, 0, 0, 99},
+			0,
 		},
 		testcase{
 			[]int{2, 3, 0, 3, 99},
 			[]int{2, 3, 0, 6, 99},
+			0,
 		},
 		testcase{
 			[]int{2, 4, 4, 5, 99, 0},
 			[]int{2, 4, 4, 5, 99, 9801},
+			0,
 		},
 		testcase{
 			[]int{1, 1, 1, 4, 99, 5, 6, 0, 99},
 			[]int{30, 1, 1, 4, 2, 5, 6, 0, 99},
+			0,
 		},
 	}
 
 	for _, c := range cases {
-		programRun(c.program)
+		if ret := programRun(c.program) != c.retcode; ret {
+			t.Errorf("programRun failed. Expected retcode: %v, got: %v",
+				c.retcode, ret)
+		}
 		for i, op := range c.program {
 			if op != c.exppected[i] {
 				t.Errorf("programRun failed. Expected: %v, got: %v",
